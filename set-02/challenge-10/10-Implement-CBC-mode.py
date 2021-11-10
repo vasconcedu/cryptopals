317601093f24f3a1ad358f8fadabe602267357ea
Eduardo Vasconcelos <esmev@protonmail.ch>
2021-11-10 14:14:43 -0300

    Started challenge 10

---
+++ set-02/challenge-10/10-Implement-CBC-mode.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append("../..") # Adds root folder to Python modules path 
+
+from my_cryptopals_utils import * 
+
+# This challenge consists in implementing
+# AES CBC mode using ECB instead of simply 
+# specifying CBC mode on cipher configuration
+
+# TODO 
