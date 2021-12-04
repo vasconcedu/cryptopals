5843085e51c1684584d86674d07c1ea01f64cfc0
Eduardo Vasconcelos <esmev@protonmail.ch>
2021-12-04 12:23:41 -0300

    Update challenge 12

--- set-02/challenge-12/12-Byte-at-a-time-ECB-decryption-Simple.py
+++ set-02/challenge-12/12-Byte-at-a-time-ECB-decryption-Simple.py
@@ -8,6 +8,8 @@ from my_cryptopals_utils import *
 # This challenge consists in breaking 
 # Byte-at-a-time ECB decryption 
 
+MODE_ORACLE_PLAINTEXT = "qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq"
+
 def __main__():
 
     print("[+] Cryptopals 12 - Byte-at-a-time ECB decryption (Simple)\n")
@@ -26,17 +28,28 @@ def __main__():
     # 2.1. Oracle calibration
     cbc_boundary = 0
     for i in range(0, 100):
-        mode, ciphertext = encryption_oracle("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
+        mode, ciphertext = encryption_oracle(MODE_ORACLE_PLAINTEXT)
         if mode == MODE_CBC:
             repeating_blocks_count = count_repeating_blocks(ciphertext)
             if cbc_boundary < repeating_blocks_count:
                 cbc_boundary = repeating_blocks_count
 
     # 2.2. Mode detection 
-    ciphertext = consistent_key_ecb_encryption("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
+    ciphertext = consistent_key_ecb_encryption(MODE_ORACLE_PLAINTEXT)
     detected_mode = "ECB" if count_repeating_blocks(ciphertext) > cbc_boundary else "CBC"
     print("[+] Detection oracle yields: {}\n".format(detected_mode))
 
-    # 3. TODO 
+    # 3.  Knowing the block size, craft an input block that is exactly 1 byte short (for instance, if the block size is 8 bytes, make "AAAAAAA"). Think about what the oracle function is going to put in that last byte position.
+
+    input_block = ""
+    for i in range(0, probable_block_size - 1):
+        input_block = input_block + "A"
+
+    # It turns out consistent_key_ecb_encryption(input_block) will put the first 
+    # byte of the mysterious appended text after input_block, resulting in the 16th
+    # position of the ciphertext containing the encrypted byte corresponding to the
+    # first byte of the mysterious appended text! 
+
+    # TODO Eureka! Implement 
 
 __main__() 
